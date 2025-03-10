a43283ba1e3469b66c6a78a1473327c6c8822b74
c221105Nehad <nehadinkiadhossain@gmail.com>
2025-03-10 22:15:50 +0600

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './sections/Header';
+import Hero from './sections/Hero';
 export const App = () => {
 
   return (
     <main className='overflow-hidden'>
       <Header />
+      <Hero />
     </main>
   )
 }
