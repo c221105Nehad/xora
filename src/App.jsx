7d2d91953f4bfa954c035c4ba8e427d5ef4c62d8
c221105Nehad <nehadinkiadhossain@gmail.com>
2025-03-16 19:02:28 +0600

    FAQ Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './sections/Header';
 import Hero from './sections/Hero';
 import Features from './sections/Features';
 import Pricing from './sections/Pricing';
+import Faq from './sections/Faq';
 export const App = () => {
 
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
     </main>
   )
 }
