f54b961a2746be0d346d296dfddda40b79742f16
c221105Nehad <nehadinkiadhossain@gmail.com>
2025-03-18 22:48:25 +0600

    Testimonials Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './sections/Hero';
 import Features from './sections/Features';
 import Pricing from './sections/Pricing';
 import Faq from './sections/Faq';
+import Testimonials from './sections/Testimonials';
+
 export const App = () => {
 
   return (
@@ -13,6 +15,8 @@ export const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
+      
     </main>
   )
 }
